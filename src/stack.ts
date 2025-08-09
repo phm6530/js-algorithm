526eaf00baf2f95e29012b9b32cc3e658dd6ce44
hm <squirrel309@naver.com>
2025-08-09 17:36:41 +0900

    linked LIst

---
+++ src/stack.ts
@@ -0,0 +1,40 @@
+import { linkedList } from "./linked-list/index.js";
+
+class Stack<T> {
+  constructor(private list: linkedList<T> = new linkedList()) {}
+
+  pop() {
+    return this.list.deleteNode(0)?.data;
+  }
+
+  push(data: T) {
+    this.list.insertNode(0, data);
+  }
+
+  peek() {
+    this.list.getNode(0);
+  }
+
+  isEmpty() {
+    return this.list.size() === 0;
+  }
+
+  printStack() {
+    return this.list.printAll();
+  }
+}
+
+const stack = new Stack();
+stack.push(0);
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.printStack());
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.isEmpty());
