214119732dad910c24b18607d94366c81e7ae693
hm <squirrel309@naver.com>
2025-08-22 21:20:06 +0900

    해시테이블 get,set 생성

--- src/hashTable.ts
+++ src/hashTable.ts
@@ -1,16 +1,61 @@
-class hashTable {
-  constructor(private dic: number = 8) {}
+class hashTable<K, V> {
+  constructor(
+    private dic: number = 8,
+    private keyMap: Array<Array<[K, V]>> = []
+  ) {
+    this.keyMap = new Array(dic);
+  }
+
+  set(key: K, value: V) {
+    let index = this.hash(key);
+    if (!this.keyMap[index!]) {
+      this.keyMap[index!] = [];
+    }
+    this.keyMap[index!]!.push([key, value]);
+  }
+
+  get(key: K) {
+    const idx = this.hash(key);
+
+    if (this.keyMap[idx]) {
+      for (let i = 0; i < this.keyMap[idx].length; i++) {
+        const pair = this.keyMap[idx][i];
+        if (pair && pair[0] === key) {
+          return pair[1];
+        }
+      }
+    }
+
+    return undefined;
+  }
 
   // Hash Table
-  hash(key: string) {
+  hash(key: K) {
+    if (key === null || key === undefined) {
+      throw new Error("key 누락 되었습니다.");
+    }
+
+    const string = key.toString();
     let total = 0;
-    for (const char of key) {
-      const val = char.charCodeAt(0) - 96;
-      total = (total + val) % this.dic;
+
+    for (let i = 0; i < Math.min(string.length, 100); i++) {
+      const char = string[i];
+      const val = char!.charCodeAt(0) - 96;
+      /**
+       * 너무 작으면 분포 효과가 약하고, 너무 크면 연산 비용이 늘어나서 비효율적.
+       * 31 소수 쓰는 이유
+       */
+      total = (total * 31 + val) % this.dic;
     }
     return total;
   }
 }
 
 const ht = new hashTable();
-console.log(ht.hash("hashtable"));
+
+ht.set("test", "test");
+ht.set("testzzf", "testzff");
+ht.set("testzzf", "tetest");
+ht.set("ㅅㅂ", "나 반환");
+const get = ht.get("ㅅㅂ");
+console.log(get);
